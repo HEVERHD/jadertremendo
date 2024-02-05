c5ab1a8b29a8466f44ddfc528bd11678a51a0be5
HEVERHD <hgelis@bancolombia.com.co>
2024-02-05 15:56:56 -0500

    updateAutoPlay

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,18 +16,18 @@ const Navbar = () => {
     window.addEventListener("scroll", changeBackground)
   return (
     <nav className={nav ? "nav active"  : "nav"}>
-         <Link to="#"  className="logo" >
+         <Link to="main"  className="logo" >
           <img src={logo} alt={logo} /></Link>
             <input className='menu-btn'  type='checkbox' id='menu-btn' />
                  <label htmlFor='menu-btn' className='menu-icon'>
                     <span className='nav-icon'/>
                  </label>
                  <ul className='menu'>
-                    <li><Link to="#"> Inicio </Link></li>
+                    <li><Link to="main"> Inicio </Link></li>
                     <li><Link to="features"> Lanzamientos </Link></li>
                     <li><Link to="#"> Offert </Link></li>
-                    <li><Link to="#"> About </Link></li>
-                    <li><Link to="#"> Contact </Link></li>
+                    <li><Link to="about"> About </Link></li>
+                    <li><Link to="contact"> Contact </Link></li>
                  </ul>
     </nav>
   )
